6df6421d05ba99d9afdc05b4a4c8232edd3c4970
2087391083 <2087391083@qq.com>
2022-09-30 11:07:18 +0800

    回退提交

--- examples/HelloWorld/index.ts
+++
@@ -1,5 +0,0 @@
-import HelloWorld from './src/HelloWorld.vue'
-
-export default HelloWorld
-
-export * from './src/HelloWorld.vue'
--- packages/components/HomeView/index.ts
+++
@@ -1,5 +0,0 @@
-import HomeView from './src/HomeView'
-
-export default HomeView
-
-export * from './src/HomeView'
